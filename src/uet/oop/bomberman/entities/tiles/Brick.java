6ee6c228a98174f0a45ab2788468c9f5c46b9dc6
Nguyen Quang Thao <nquangthao19072004@gmail.com>
2023-04-06 21:36:51 +0700

    add animation brick explode

--- src/uet/oop/bomberman/entities/tiles/Brick.java
+++ src/uet/oop/bomberman/entities/tiles/Brick.java
@@ -1,15 +1,35 @@
 package uet.oop.bomberman.entities.tiles;
 
 import javafx.scene.image.Image;
+import uet.oop.bomberman.GameManagement;
 import uet.oop.bomberman.entities.Entity;
+import uet.oop.bomberman.graphics.Sprite;
 
 public class Brick extends Entity {
+  private boolean isExploded = false;
+  private final int MAX_TIME_ANIMATION = 6000;
+  private int timeAnimation = 0;
+  private int timeRunAnimation = 40;
   public Brick(int xUnit, int yUnit, Image img) {
     super(xUnit, yUnit, img);
   }
 
   @Override
   public void update() {
+    if (isExploded) {
+      exploded();
+    }
+  }
 
+  void exploded() {
+    timeAnimation = (timeAnimation > MAX_TIME_ANIMATION) ? 0 : timeAnimation + 1;
+    super.img = (Sprite.movingSprite(Sprite.brick_exploded, Sprite.brick_exploded1, Sprite.brick_exploded2, timeAnimation, timeRunAnimation)).getFxImage();
+    if (timeAnimation > 80) {
+      GameManagement.getStillObjects().remove(this);
+    }
+  }
+
+  public void setExploded() {
+    isExploded = true;
   }
 }
